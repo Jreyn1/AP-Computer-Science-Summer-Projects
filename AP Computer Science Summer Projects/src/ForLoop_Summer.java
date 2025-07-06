3db6a46d319614e838465613bba6517db13cf863
Computer <Computer@JTPC>
2025-07-06 16:22:52 -0600

    ForLoopSummer Complete

---
+++ AP Computer Science Summer Projects/src/ForLoop_Summer.java
@@ -0,0 +1,27 @@
+
+public class ForLoop_Summer
+	{
+
+		public static void main(String[] args)
+			{
+				int number = 0;
+				for (int i = 0; i < 10 ; i++)
+					{
+						number += 1;
+						System.out.println(number);
+					}
+				
+				//For Loop Block Test
+				//System.out.println("");
+				
+				
+				for (int i = 10; i > 0 ; i--)
+					{
+						
+						System.out.println(number);
+						number -= 1;
+					}
+
+			}
+
+	}
